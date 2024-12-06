9ef9b2c803700cd5b551e5c7eec5260062b7ad9b
Mustapha Muhammed <129334579+mustapha2200@users.noreply.github.com>
2024-12-06 00:49:00 +0000

    Delete dist directory

--- dist/routes/conversionRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const conversionController_1 = require("../controllers/conversionController");
-const router = (0, express_1.Router)();
-router.post('/convert', conversionController_1.convertText);
-router.get('/health', conversionController_1.healthCheck);
-exports.default = router;
